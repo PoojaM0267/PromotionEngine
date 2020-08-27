a14ee7bb397c5a6f1b1142c032c0ba0df164f7e9
Pooja Mahtha <pooja0267@gmail.com>
2020-08-27 19:19:49 +0530

    test project added

---
+++ PromotionEngine/PromotionEngine/Controllers/PromotionEngineController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PromotionEngine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionEngineController : ControllerBase
+    {
+        [HttpGet("checkoutprice")]
+        public ActionResult<int> CheckOutPrice(string inputSkuIds)
+        {
+            return Ok();
+        }
+    }
+}
